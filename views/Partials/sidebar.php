aa59c414e9c8b7a588c4364a2fcddf9b96620ded
Felipe Mogollón <94751785+FelipeMogollon1@users.noreply.github.com>
2024-11-20 17:42:17 -0500

    feat: update sidebar

--- views/Partials/sidebar.php
+++ views/Partials/sidebar.php
@@ -8,29 +8,20 @@ require_once __DIR__ . '/../../functions/UrlHelper.php';
     </a>
     <hr>
     <ul class="nav nav-pills flex-column mb-auto">
+
         <li class="nav-item">
-            <a href="<?php echo base_url(); ?>/Judge" class="nav-link active" aria-current="page">
+            <a href="<?php echo base_url(); ?>/judges" class="nav-link active" aria-current="page">
                 <svg class="bi pe-none me-2" width="16" height="16"><use xlink:href="#house"></use></svg>
                 Jueces
             </a>
         </li>
+
         <li class="nav-item">
-            <a href="<?php echo base_url(); ?>/users/" class="nav-link active" aria-current="page">
-                <svg class="bi pe-none me-2" width="16" height="16"><use xlink:href="#home"></use></svg>
-                Users
+            <a href="<?php echo base_url(); ?>/sports" class="nav-link active" aria-current="page">
+                <svg class="bi pe-none me-2" width="16" height="16"><use xlink:href="#house"></use></svg>
+                Deportes
             </a>
         </li>
-        <li>
-            <a href="<?php echo base_url(); ?>/cities/" class="nav-link text-white">
-                <svg class="bi pe-none me-2" width="16" height="16"><use xlink:href="#speedometer2"></use></svg>
-                Cities
-            </a>
-        </li>
-        <li>
-            <a href="<?php echo base_url(); ?>/jobs/" class="nav-link text-white">
-                <svg class="bi pe-none me-2" width="16" height="16"><use xlink:href="#table"></use></svg>
-                Jobs
-            </a>
-        </li>
+
     </ul>
 </div>
