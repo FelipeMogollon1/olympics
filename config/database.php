46864863cce61c9a207b27ce715f6d7076732cbe
Felipe Mogollón <94751785+FelipeMogollon1@users.noreply.github.com>
2024-11-20 15:30:31 -0500

    fix: update connection database pgsql

--- config/database.php
+++ config/database.php
@@ -1,25 +1,32 @@
 <?php
 
 class Database {
-    private $host = 'localhost';
-    private $db_name = 'CompanyDB';
-    private $username = 'root';
-    private $password = '';
+    private $host = '3.85.233.186';
+    private $db_name = 'olimpicos';
+    private $username = 'admin1';
+    private $password = 'L{@n<:c%Da,_';
+    private $port = '5432';
+    private $schema = 'equipo1';
     public $conn;
 
     public function getConnection() {
         $this->conn = null;
         try {
-            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
+            $this->conn = new PDO(
+                "pgsql:host={$this->host};port={$this->port};dbname={$this->db_name}",
+                $this->username,
+                $this->password
+            );
             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             $this->conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 
-        } catch(PDOException $exception) {
-
-            error_log("Error of connection: " . $exception->getMessage());
-            echo "An error occurred while connecting to the database.";
+            $this->conn->exec("SET search_path TO {$this->schema}");
+        } catch (PDOException $exception) {
+            error_log("Error de conexión: " . $exception->getMessage());
+            echo "Ocurrió un error al conectar con la base de datos: " . $exception->getMessage();
         }
         return $this->conn;
     }
+
 }
 ?>
